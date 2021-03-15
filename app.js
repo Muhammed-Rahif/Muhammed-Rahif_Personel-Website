c36d45ab4f391efed01cc1b2989c8dac3ec85b86
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-03-15 16:40:28 +0530

    Website refreshed and updated

--- app.js
+++ app.js
@@ -38,14 +38,14 @@ $(document).ready(function(){
   
       // typing text animation script
       var typed = new Typed(".typing", {
-          strings: ["Web Developer", "Student", "Freelancer"],
+          strings: ["Freelancer", "Web Designer", "Web Developer","Mobile Developer"],
           typeSpeed: 100,
           backSpeed: 60,
           loop: true
       });
   
       var typed = new Typed(".typing-2", {
-          strings: [ "Web Developer", "Student", "Freelancer"],
+          strings: [ "Freelancer", "Web Designer", "Web Developer","Mobile Developer"],
           typeSpeed: 100,
           backSpeed: 60,
           loop: true
